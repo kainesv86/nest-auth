5ceba4d32ea44efe38bbf93f95ca70b42482a07e
unknown <kainesv86@gmail.com>
2021-12-25 14:30:11 +0700

    Add - User Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+
+const DBConfig = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '1234567890Aa',
+  database: 'nest_auth',
+  synchronize: true,
+  keepConnectionAlive: true,
+  entities: [],
+});
 
 @Module({
   imports: [AuthModule],
