19430cef48909aab9134a319f71a46aacd031dc0
unknown <kainesv86@gmail.com>
2021-12-29 17:44:12 +0700

    Update Convert Vietnamese langauge

--- utils/validator/schema/user.validator.ts
+++ utils/validator/schema/user.validator.ts
@@ -1,10 +1,17 @@
 import * as Joi from 'joi';
+import { StringVnLang } from 'joi-vn-lang';
 import { User } from 'src/auth/entities/user.entity';
 
 export function userJoiSchema(field: keyof User) {
   switch (field) {
     case 'username':
-      return Joi.string().min(5).max(40).trim().lowercase().required();
+      return Joi.string()
+        .min(5)
+        .max(40)
+        .trim()
+        .lowercase()
+        .required()
+        .messages(StringVnLang);
     case 'password':
       return Joi.string()
         .min(5)
@@ -12,8 +19,9 @@ export function userJoiSchema(field: keyof User) {
         .trim()
         .lowercase()
         .alphanum()
-        .required();
+        .required()
+        .messages(StringVnLang);
     case 'email':
-      return Joi.string().min(5).max(40).email();
+      return Joi.string().min(5).max(40).email().messages(StringVnLang);
   }
 }
